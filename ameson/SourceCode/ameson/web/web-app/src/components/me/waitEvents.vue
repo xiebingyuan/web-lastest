d7260573a900f9bbca5a9c26b34d139ff21a072b
xieby <xieby@0b44b224-c366-4313-8c14-14be4649c538>
2018-09-21 06:19:16 +0000

    我的待办 & 获取userId方法

--- ameson/SourceCode/ameson/web/web-app/src/components/me/waitEvents.vue
+++ ameson/SourceCode/ameson/web/web-app/src/components/me/waitEvents.vue
@@ -13,9 +13,9 @@
         </thead>
         <tbody>
         	<tr v-for="(info,index) in infos" track-by="$index">
-        		<td>{{info.serOrderType}}</td>
+        		<td>{{info.typeName}}</td>
         		<td>{{info.serOrderDesc}}</td>
-        		<td><x-button mini plain type="primary" @click.native="process(info.serOrderNum)">处理</x-button></td>
+        		<td><x-button mini plain type="primary" @click.native="process(info)">处理</x-button></td>
         	</tr>	
         </tbody>	
 			</x-table>	
@@ -25,7 +25,6 @@
 
 <script type="text/javascript">
   import { XButton, XHeader, XTable, Group, XInput, Cell } from 'vux'
-  // import qs from 'qs'
   export default {
     components: {
       XHeader,
@@ -39,11 +38,24 @@
       return {
         name: '我的待办',
         infos: {},
+        typeMap: [],
         pageNo: 1,
         pageSize: 20
       }
     },
     mounted () {
+      var orderData = new Map()
+      var dictAll = this.commonJs.getDictInfo()
+      for (var i = 0; i < dictAll.length; i++) {
+        let dict = dictAll[i]
+        // 工单问题类型
+        if (dict.dictParentId === 110) {
+          let key = dict.dictTypeValue
+          let name = dict.dictTypeCname
+          orderData.set(key, name)
+        }
+      }
+      this.typeMap = orderData
       this.query(this.pageSize, this.pageNo)
     },
     methods: {
@@ -51,14 +63,14 @@
         let res = await this.$http.postDeviceQuery('/serviceOrder/getWaitHandleList', {}, pageSize, pageNo)
         if (res.code === 0) {
           this.infos = res.data
+          for (var i = this.infos.length - 1; i >= 0; i--) {
+            this.infos[i].typeName = this.typeMap.get(parseInt(this.infos[i].serOrderType))
+          }
         }
       },
-      sync () {
-        console.info('22')
-      },
-      process (orderId) {
-        console.info(orderId)
-        this.$router.push({path: '/standard/edit', query: { serOrderNum: orderId }})
+      process (info) {
+        console.info(info)
+        // this.$router.push({path: '/standard/edit', query: { serOrderNum: orderId }})
       }
     }
   }
