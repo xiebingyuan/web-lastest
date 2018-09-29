4032cd234b34000560cf21bf01356422a303a623
xieby <xieby@0b44b224-c366-4313-8c14-14be4649c538>
2018-09-29 06:05:45 +0000

    ip 换 网址

--- ameson/SourceCode/ameson/web/web-app/src/utils/commonJs.js
+++ ameson/SourceCode/ameson/web/web-app/src/utils/commonJs.js
@@ -9,7 +9,7 @@ export default {
   },
   initDict: function () {
     // Vue.prototype.$http.post('http://47.100.239.45:8661/getAllDict', {token: 'OIPI-89-8876-DAD-8686-JOIJIO-098786'}).then(function (response) {
-    Vue.prototype.$http.post('http://www.zjytech.cn/getAllDict', {token: 'OIPI-89-8876-DAD-8686-JOIJIO-098786'}).then(function (response) {
+    Vue.prototype.$http.post('http://www.zjytech.cn:8661/getAllDict', {token: 'OIPI-89-8876-DAD-8686-JOIJIO-098786'}).then(function (response) {
       // console.log(response)
       Vue.prototype.GLOBAL.dictInfo = response
       if (window.localStorage) {
